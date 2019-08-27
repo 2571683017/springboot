33de79387d1fb5ce83dd8e336bde8c4478c21138
dong <2571683017@qq.com>
2019-08-27 23:02:37 +0800

    d  2

--- src/main/java/com/qfedu/springboot/controller/Login.java
+++ src/main/java/com/qfedu/springboot/controller/Login.java
@@ -3,6 +3,6 @@ package com.qfedu.springboot.controller;
 public class Login {
 
     public static void main(String[] args) {
-        System.out.println("helloWord");
+        System.out.println("123");
     }
 }
