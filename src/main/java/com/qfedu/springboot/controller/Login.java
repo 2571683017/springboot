59f6524bfdf1a175534078b4693bc3c7d869f277
dong <2571683017@qq.com>
2019-08-28 19:33:11 +0800

    d  6

--- src/main/java/com/qfedu/springboot/controller/Login.java
+++ src/main/java/com/qfedu/springboot/controller/Login.java
@@ -4,5 +4,8 @@ public class Login {
 
     public static void main(String[] args) {
         System.out.println("1231456789");
+        System.out.println(987654321);
+
+        System.out.println("王五");
     }
 }
